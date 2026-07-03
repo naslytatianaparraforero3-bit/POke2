cebd5f869e265795566c92c608c56511ddb3e524
Nasly Parra <naslytatianaparraforero3@gmail.com>
2026-07-03 08:51:37 -0500

    se arregla nombre

--- src/app/features/pokemon/components/pokemon-card/pokemon-card.component.ts
+++ src/app/features/pokemon/components/pokemon-card/pokemon-card.component.ts
@@ -1,9 +1,9 @@
 import { Component, Input } from '@angular/core';
-import { Pokemon } from '../../model/pokemon.model';
+import { Pokemon, PokemonStat } from '../../model/pokemon.model';
 
 @Component({
   selector: 'app-pokemon-card',
-   standalone: false,
+  standalone: false,
   templateUrl: './pokemon-card.component.html',
   styleUrls: ['./pokemon-card.component.scss']
 })
@@ -15,7 +15,7 @@ export class PokemonCardComponent {
   }
 
   getStatValue(statName: string): number {
-    const stat = this.pokemon.stats.find(s => s.stat.name === statName);
+    const stat = this.pokemon.stats.find((s: PokemonStat) => s.stat.name === statName);
     return stat ? stat.base_stat : 0;
   }
 }
