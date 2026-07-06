d86ee53444fb8b9947288a1df15ec416770a6617
devstiven12 <devstiven12@gmail.com>
2026-07-06 18:41:02 -0500

    feat: agrego ReactiveFormsModule para búsqueda con debounce

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';  
 
 import { AppComponent } from './app.component';
 import { PokemonModule } from './features/pokemon/pokemon.module';
@@ -12,6 +13,7 @@ import { PokemonModule } from './features/pokemon/pokemon.module';
   imports: [
     BrowserModule,
     HttpClientModule,
+    ReactiveFormsModule,
     PokemonModule
   ],
   providers: [],
